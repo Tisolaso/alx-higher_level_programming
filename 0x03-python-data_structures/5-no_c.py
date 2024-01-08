aa3935aa55c9983f3f7eee08574419d184e3a038
Tisolaso <spanya2017@gmail.com>
2024-01-08 14:14:26 -0800

    lol

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-
     for char in my_string:
-        if char in ["c", "C"]:
-            continue
-
-        new_string += char
+        if char != 'c' and char != 'C':
+            new_string += char
     return new_string
