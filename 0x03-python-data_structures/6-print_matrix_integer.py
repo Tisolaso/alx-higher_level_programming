eb926044b7aa5ae760f51613f8ce91f956ec8cba
Tisolaso <spanya2017@gmail.com>
2024-01-08 12:37:32 -0800

    lol

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for index, int in enumerate(list):
             print("{:d}".format(int), end="")
-            print("{:s}".format(" " if index != (len(list)-1) else ""), end="")
+            print("{:s}".format(" " if index != (len(list)-1) else "\n"), end="")
