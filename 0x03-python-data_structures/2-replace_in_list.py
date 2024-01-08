89e45a8bc4f0a0efeb280f83228bf7c0a7361f39
Tisolaso <spanya2017@gmail.com>
2024-01-08 10:22:33 -0800

    lol

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or len(my_list) <= idx:
         return my_list
 
-    new_list = my_list.copy()
+    new_list = my_list
 
     new_list[idx] = element
 
