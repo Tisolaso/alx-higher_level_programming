0b5cc8a1e64738e6ba64d57ee47e571b7cd58c88
Tisolaso <spanya2017@gmail.com>
2024-01-12 14:08:42 -0800

    lol

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    index = 0
+    if roman_string is not None:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        listrom = list(roman.keys())
+        length = len(roman_string)
+        for char in roman_string:
+            # print("++ "+char)
+            if char in roman:
+                # print("+ "+char, roman[char])
+                if char in 'IXC':
+                    if index < length-1:
+                        if listrom[index+1] in 'VXLCDM':
+                            sum = sum + (roman[listrom[index+1]]-roman[char])
+                sum += roman[char]
+            index += 1
+    return sum
