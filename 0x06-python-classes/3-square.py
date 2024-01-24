ce88612dadb5382f4da4d9826225d5aa13166b50
Tisolaso <spanya2017@gmail.com>
2024-01-24 11:22:58 -0800

    lol

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """Square class with validated private instance attribute size"""
+    """Simple square class with his size as a field"""
 
     def __init__(self, size=0):
-        """Args:
-               size: size of square
-        """
-        if type(size) is not int:
+        """ Instance the class Square
+            Arguments:
+                @size: the size of every side of the Square,
+                        it must be a positive integer value"""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -16,5 +15,10 @@ class Square:
             self.__size = size
 
     def area(self):
-        """Returns the calculated the area of Square instance"""
-        return self.__size ** 2
+        """ Compute the area of a square
+            with the formula:
+                                area = @size ^ 2 = @size * @size
+            Return:
+                    Power of the Square size to 2
+                    or size multiplicated by size."""
+        return (self.__size ** 2)
