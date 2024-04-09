b5f2b8d9acb5c3de8a6ba7f786fe9faae8d967b1
Youssef <spanya2017@gmail.com>
2024-04-08 18:42:06 -0700

    lol

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const sorted = args.slice(0);
 
-if (args.length > 2) {
+if (args.length >= 2) {
   sorted.sort((a, b) => b - a);
   console.log(sorted[1]);
 } else {
