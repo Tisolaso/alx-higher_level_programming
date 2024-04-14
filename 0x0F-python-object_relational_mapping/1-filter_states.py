7dcb0ca89661e04b8c18cc03807d1e3d879abec9
Youssef <spanya2017@gmail.com>
2024-04-14 15:30:42 -0700

    lol

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
+"""
+states models
+"""
 if __name__ == "__main__":
-        from MySQLdb import _mysql
-        import sys
+    from MySQLdb import _mysql
+    import sys
 
-        args = sys.argv[1:]
+    args = sys.argv[1:]
 
-        db_config = {
-                'host': 'localhost',
-                'port': 3306,
-                'user': args[0],
-                'password': args[1],
-                'database': args[2],
-        }
-        db = _mysql.connect(**db_config)
-        db.query(""" SELECT * FROM states WHERE name LIKE 'N%' """)
+    db_config = {
+            'host': 'localhost',
+            'port': 3306,
+            'user': args[0],
+            'password': args[1],
+            'database': args[2],
+            }
+    db = _mysql.connect(**db_config)
+    db.query(""" SELECT * FROM states WHERE name LIKE 'N%' """)
 
-        r = db.store_result()
+    r = db.store_result()
 
-        r = r.fetch_row(maxrows=0)
+    r = r.fetch_row(maxrows=0)
 
-        for row in r:
-            print(f"({row[0].decode()}, '{row[1].decode()}')")
+    for row in r:
+        print(f"({row[0].decode()}, '{row[1].decode()}')")
         db.close()
-
